d5a29e107c2758f16999c7cebf90227d671d5515
Kamran Shahid <kamiy2j@gmail.com>
2025-06-07 04:44:43 +0500

    changes

--- security_groups.tf
+++ security_groups.tf
@@ -50,6 +50,22 @@ resource "aws_security_group" "ec2" {
     security_groups = [aws_security_group.alb.id]
   }
 
+
+# BI instance needs public access for SSL
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port       = 3000
     to_port         = 3000
@@ -75,52 +91,6 @@ resource "aws_security_group" "ec2" {
     Name = "kamran-ec2-sg"
   }
 }
-
-  # BI Tool Security Group
-  resource "aws_security_group" "bi_tool" {
-    name        = "kamran-bi-sg"
-    description = "Security group for BI Tool instance"
-    vpc_id      = aws_vpc.main.id
-
-    ingress {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-      from_port   = 5000
-      to_port     = 5000
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-      Name = "kamran-bi-sg"
-    }
-  }
 
 # RDS MySQL Security Group
 resource "aws_security_group" "rds_mysql" {
